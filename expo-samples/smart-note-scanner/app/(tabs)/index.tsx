a87f71deab23bb601e9e0ba5f50ee8ea343b6851
xulihang <xulihanghai@163.com>
2024-07-15 13:48:47 +0800

    implement document scanner for react native

--- expo-samples/smart-note-scanner/app/(tabs)/index.tsx
+++ expo-samples/smart-note-scanner/app/(tabs)/index.tsx
@@ -1,26 +1,70 @@
-import { Image, StyleSheet, Text, Button, View } from 'react-native';
+import { Image, StyleSheet, Text, Button, View, BackHandler } from 'react-native';
 
 import ParallaxScrollView from '@/components/ParallaxScrollView';
+import { useEffect, useState } from 'react';
+import DocumentScanner from '@/components/Scanner';
 
 export default function HomeScreen() {
+  const [isScanning,setIsScanning] = useState(false);
+  const [imageDataURL,setImageDataURL] = useState("");
+  useEffect(() => {
+    BackHandler.addEventListener('hardwareBackPress', function () {
+      if (!isScanning) {
+        /**
+         * When true is returned the event will not be bubbled up
+         * & no other back action will execute
+         */
+        setIsScanning(false);
+        return true;
+      }
+      /**
+       * Returning false will let the event to bubble up & let other event listeners
+       * or the system's default back action to be executed.
+       */
+      return false;
+    });
+  }, []);
+  
   return (
-    <ParallaxScrollView
-      headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
-      headerImage={
-        <Image
-          source={require('@/assets/images/partial-react-logo.png')}
-          style={styles.reactLogo}
-        />
-      }>
-      <View>
-        <Text>Smart Note Scanner</Text>
-      </View>
-      <Button title="Start Scanning"></Button>
-    </ParallaxScrollView>
+    <>
+      {isScanning && (
+        <DocumentScanner
+          license="DLS2eyJoYW5kc2hha2VDb2RlIjoiMTAwMjI3NzYzLVRYbFhaV0pRY205cSIsIm1haW5TZXJ2ZXJVUkwiOiJodHRwczovL21sdHMuZHluYW1zb2Z0LmNvbSIsIm9yZ2FuaXphdGlvbklEIjoiMTAwMjI3NzYzIiwic3RhbmRieVNlcnZlclVSTCI6Imh0dHBzOi8vc2x0cy5keW5hbXNvZnQuY29tIiwiY2hlY2tDb2RlIjotMzg1NjA5MTcyfQ=="
+          onClosed={()=>{setIsScanning(false)}}
+          onScanned={(dataURL:string)=>{setImageDataURL(dataURL)}}
+        ></DocumentScanner>
+      )}
+      {!isScanning && (
+        <ParallaxScrollView
+          headerBackgroundColor={{ light: '#A1CEDC', dark: '#1D3D47' }}
+          headerImage={
+            <Image
+              source={require('@/assets/images/partial-react-logo.png')}
+              style={styles.reactLogo}
+            />
+          }>
+          <View>
+            <Text>Smart Note Scanner</Text>
+          </View>
+          <Button title="Start Scanning" onPress={()=>{
+            setIsScanning(true);
+          }}></Button>
+          {imageDataURL && (
+            <Image
+              source={{uri:imageDataURL}}
+              style={styles.scanned}
+            />  
+          )}
+        </ParallaxScrollView>
+      )}
+    </>
   );
 }
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   reactLogo: {
     height: 178,
     width: 290,
@@ -28,4 +72,8 @@ const styles = StyleSheet.create({
     left: 0,
     position: 'absolute',
   },
+  scanned:{
+    width: "100%",
+    resizeMode: "contain"
+  }
 });
---
+++ expo-samples/smart-note-scanner/components/Scanner.tsx
@@ -0,0 +1,58 @@
+import { StyleSheet, Text } from 'react-native';
+import { useEffect,useState } from 'react';
+import { Camera } from 'expo-camera';
+import WebView from 'react-native-webview';
+import { SafeAreaView } from 'react-native-safe-area-context';
+export interface ScannerProps {
+  license?:string;
+  onScanned?:(dataURL:string) => void;
+  onClosed?:() => void;
+}
+export default function DocumentScanner(props:ScannerProps) {
+  const [hasPermission, setHasPermission] = useState(false);
+  useEffect(() => {
+    (async () => {
+      const response  = await Camera.requestCameraPermissionsAsync();
+      setHasPermission(response.granted);
+    })();
+  }, []);
+  const getURI = () => {
+    let URI = 'https://tony-xlh.github.io/dynamsoft-capture-vision-samples/smart-note-scanner.html?startScan=true';
+    if (props.license) {
+      URI = URI + "&license="+props.license;
+    }
+    return URI;
+  }
+  if (hasPermission) {
+    return (
+      <SafeAreaView style={StyleSheet.absoluteFill}>
+        <WebView
+          style={styles.webview}
+          allowsInlineMediaPlayback={true}
+          mediaPlaybackRequiresUserAction={false}
+          onMessage={(event) => {
+            if (!event.nativeEvent.data) {
+              if (props.onClosed) {
+                props.onClosed();
+              }
+            }else{
+              if (props.onScanned) {
+                const dataURL = event.nativeEvent.data;
+                props.onScanned(dataURL);
+              }
+            }
+          }}
+          source={{ uri: getURI() }}
+        />
+      </SafeAreaView>
+    );
+  }else{
+    return <Text>No permission.</Text>
+  }
+}
+
+const styles = StyleSheet.create({
+  webview:{
+    flex: 1,
+  }
+});
